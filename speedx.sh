ae0d4fc40660b21d2082123f715ac11c8f73c2e5
tgzin <75448283+tg230@users.noreply.github.com>
2023-11-06 20:37:10 -0300

    Update speedx.sh

--- speedx.sh
+++ speedx.sh
@@ -245,7 +245,7 @@ esac
 }
 
 update_check() {
-if ping -q -c 1 -W 1 google.com &> /dev/null; then
+
 versao_arquivo=$(curl -s -L https://raw.githubusercontent.com/tg230/teste2222/main/vers%C3%A3o.txt)
 versao_script="1.0.0"
 clear
@@ -269,11 +269,7 @@ else
     sleep 1.0
     update_menu
 fi
-else
-    echo "Sem conexão à internet. Não é possível verificar atualizações."
-    sleep 2.0
-    update_menu
-fi
+
 }
 
 home() {
